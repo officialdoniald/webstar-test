8412fbbb6e9beefab52f0f6dd1b58089d64f56de
blenartappset <bence960206@gmail.com>
2022-12-15 17:38:54 +0100

    feat: seasons_screen defined

---
+++ lib/screens/seasons_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeasonsScreen extends StatefulWidget {
+  const SeasonsScreen({super.key});
+
+  @override
+  State<SeasonsScreen> createState() => _SeasonsScreenState();
+}
+
+class _SeasonsScreenState extends State<SeasonsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
