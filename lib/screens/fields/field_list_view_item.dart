006c4f030877217e40940d9197dc0a6a9ef20621
blenartappset <bence960206@gmail.com>
2022-12-17 21:10:23 +0100

    feat: field list, navigation to wiki

--- lib/screens/fields/field_list_view_item.dart
+++ lib/screens/fields/field_list_view_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webstartest/services/models/driver_dto.dart';
 
 class FieldListViewItem extends StatelessWidget {
@@ -12,8 +14,52 @@ class FieldListViewItem extends StatelessWidget {
     required this.context,
   });
 
+  _infoClicked() async {
+    if (model.url == null) {
+      return;
+    }
+
+    if (await canLaunchUrl(Uri.parse(model.url!))) {
+      await launchUrl(Uri.parse(model.url!));
+    }
+  }
+
+  _navToFieldsScreen() {
+    /*Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FieldsScreen(season: model.season),
+      ),
+    );*/
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text(
+                  model.familyName ?? "-",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: _infoClicked,
+              child: SvgPicture.asset('assets/images/info.svg'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
