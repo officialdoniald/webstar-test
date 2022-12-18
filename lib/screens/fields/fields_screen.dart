7d061cacf13d5584cde122b93eb46a3a0619dd48
blenartappset <bence960206@gmail.com>
2022-12-18 10:45:00 +0100

    feat: fields_screen nationalities list implemented

--- lib/screens/fields/fields_screen.dart
+++ lib/screens/fields/fields_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:webstartest/services/models/driver_dto.dart';
@@ -27,6 +30,59 @@ class _FieldsScreenState extends State<FieldsScreen> {
 
   _back() {
     Navigator.pop(context);
+  }
+
+  Column _drawDriverNationalityStatistic(List<Drivers> drivers) {
+    var driversMap = groupBy(drivers.map((driver) => driver.toJson()).toList(),
+        (Map obj) => obj['nationality']);
+
+    driversMap = Map.fromEntries(driversMap.entries.toList()
+      ..sort((e1, e2) => e2.value.length.compareTo(e1.value.length)));
+
+    List<Widget> natList = [
+      Text(
+        getTranslated(context, "fields_screen_nationalities"),
+        style: const TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ];
+
+    driversMap.forEach(
+      (nationality, list) {
+        print('$nationality: ${list.length}');
+
+        natList.add(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$nationality: ",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                list.length.toStringAsFixed(0),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: natList,
+    );
   }
 
   @override
@@ -71,15 +127,29 @@ class _FieldsScreenState extends State<FieldsScreen> {
                 snapshot.data!.mRData != null &&
                 snapshot.data!.mRData!.driverTable != null &&
                 snapshot.data!.mRData!.driverTable!.drivers != null) {
-              return ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: snapshot.data!.mRData!.driverTable!.drivers!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return FieldListViewItem(
-                    model: snapshot.data!.mRData!.driverTable!.drivers![index],
-                    context: context,
-                  );
-                },
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _drawDriverNationalityStatistic(
+                        snapshot.data!.mRData!.driverTable!.drivers!),
+                    ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount:
+                          snapshot.data!.mRData!.driverTable!.drivers!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return FieldListViewItem(
+                          model: snapshot
+                              .data!.mRData!.driverTable!.drivers![index],
+                          context: context,
+                        );
+                      },
+                    ),
+                  ],
+                ),
               );
             } else {
               return Column(
